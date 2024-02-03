4ba24a70350f77c7a725bb136a797c332a80440c
UBC Student <yzy98@students.cs.ubc.ca>
2024-02-03 12:58:55 -0800

    Updated api functionality unit test for PUT/POST (#29)
    Co-authored-by: UBC Student <lymeng02@students.cs.ubc.ca>

--- test/apiUnitTest.java
+++ test/apiUnitTest.java
@@ -1,33 +1,56 @@
-// import static org.junit.Assert.assertEquals;
-// import ast.api.*;
-// import org.junit.Test;
+import static org.junit.Assert.*;
 
-// import java.net.http.HttpResponse;
+import controller.APIService;
+import org.json.JSONObject;
+import org.junit.Test;
 
-// public class apiUnitTest {
-//     @Test
-//     public void testSimplePUT() {
-//         PUT put = new PUT();
-//         put.setURI("https://8k06e.wiremockapi.cloud/json/2");
-//         put.setParams("{\n" +
-//                 "  \"id\": 12345,\n" +
-//                 "  \"value\": \"abc-def-ghi\"\n" +
-//                 "}");
-//         HttpResponse<String> res;
-//         res = put.sendPut();
-//         assertEquals(200, res.statusCode());
-//     }
 
-//     @Test
-//     public void testSimplePOST() {
-//         POST post = new POST();
-//         post.setURI("https://8k06e.wiremockapi.cloud/json");
-//         post.setParams("{\n" +
-//                 "  \"id\": 12345,\n" +
-//                 "  \"value\": \"abc-def-ghi\"\n" +
-//                 "}");
-//         HttpResponse<String> res;
-//         res = post.sendPost();
-//         assertEquals(201, res.statusCode());
-//     }
-// }
+public class apiUnitTest {
+    @Test
+    public void testSimpleGET() {
+
+    }
+
+    @Test
+    public void testSimpleDELETE() {
+
+    }
+
+    @Test
+    public void testSimplePUT() {
+        // wiremock api from Ricky's setup
+        APIService api = new APIService();
+        JSONObject json = new JSONObject("{\n" +
+                "  \"id\": 12345,\n" +
+                "  \"value\": \"abc-def-ghi\"\n" +
+                "}");
+        try
+        {
+            Object res = api.makePutRequest("https://8k06e.wiremockapi.cloud/json/2", json);
+            assertEquals("{\"message\":\"Success\"}", res.toString());
+        }
+        catch (Exception e)
+        {
+            assertTrue(false);
+        }
+    }
+
+    @Test
+    public void testSimplePOST() {
+        // wiremock api from Ricky's setup
+        APIService api = new APIService();
+        JSONObject json = new JSONObject("{\n" +
+                "  \"id\": 12345,\n" +
+                "  \"value\": \"abc-def-ghi\"\n" +
+                "}");
+        try
+        {
+            Object res = api.makePostRequest("https://8k06e.wiremockapi.cloud/json", json);
+            assertEquals("{\"message\":\"Success\"}", res.toString());
+        }
+        catch (Exception e)
+        {
+            assertTrue(false);
+        }
+    }
+}
