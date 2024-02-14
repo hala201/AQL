66cb7fb3c8b64c233dcd2994f0bdcbeae6c767f3
UBC Student <lymeng02@students.cs.ubc.ca>
2024-02-13 23:55:59 -0800

    Simple GUI (#47)
    * simple ui
    
    * refactor and simple auto indent with braces
    
    * deindent on }
    
    * use rsyntax
    
    * change frame name

--- src/main/main.java
+++ src/main/main.java
@@ -1,49 +1,11 @@
 package main;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.TokenStream;
-
-import ast.Node;
-import controller.AQLVisitor;
-import controller.Evaluator;
-import controller.SyntaxErrorListener;
-import gen.AQLLexer;
-import gen.AQLParser;
 
 public class main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
-        String aqlInput = """
-        GET https://65y4r.wiremockapi.cloud/admins/{users.id}
-        GET https://65y4r.wiremockapi.cloud/users
-        DELETE https://65y4r.wiremockapi.cloud/users
-        """;
-        AQLLexer lexer = new AQLLexer(CharStreams.fromString(aqlInput));
-        System.out.println(lexer.getAllTokens());
-        lexer.reset();
-
-        TokenStream tokens = new CommonTokenStream(lexer);
-        PrintWriter out = new PrintWriter(new FileWriter("output.txt"));
-        SyntaxErrorListener errorListener = new SyntaxErrorListener(out);
-        
-        AQLParser parser = new AQLParser(tokens);
-        parser.addErrorListener(errorListener);
-        AQLVisitor visitor = new AQLVisitor();
-        Node parsedProgram = parser.program().accept(visitor);
-
-        if (!(errorListener.hasError())) {
-            System.out.println("No syntax errors found.");
-            // System.out.println(parser.program().toStringTree(parser));
-            parsedProgram.accept(new Evaluator(), out);
-        } else {
-            System.out.println("Syntax errors found.");
-        }
-
-        out.close();
+        new UI();
     }
+    
 }
